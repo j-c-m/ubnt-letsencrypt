be09fef76c829fef50fe81489032cb1ea669fc43
Jesse Miller <jmiller@jmiller.com>
2017-02-24 16:25:35 -0600

    Fix WAN device substitution.

--- scripts/renew.acme.sh
+++ scripts/renew.acme.sh
@@ -10,7 +10,7 @@ DOMAIN=$1
 WAN=$2
 
 ACMEHOME=/config/.acme.sh
-WANIP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+WANIP=$(ip addr show $WAN | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
 /sbin/iptables -I WAN_LOCAL 2 -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j RETURN
 $ACMEHOME/acme.sh --issue -d $DOMAIN --standalone --home $ACMEHOME --local-address $WANIP --keypath /tmp/server.key --fullchainpath /tmp/full.cer --reloadcmd /config/scripts/reload.acme.sh
