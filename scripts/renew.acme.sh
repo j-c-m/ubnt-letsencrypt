e6cd9d6ccc10029511d5331cf16f409365f351e5
Jesse Miller <jmiller@jmiller.com>
2017-03-17 14:47:54 -0500

    Use webroot and lighttpd instead of standalone
    The netcat version included with base system somtimes had issues with
    extraneous options.

--- scripts/renew.acme.sh
+++ scripts/renew.acme.sh
@@ -12,6 +12,26 @@ WAN=$2
 ACMEHOME=/config/.acme.sh
 WANIP=$(ip addr show $WAN | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
+mkdir -p $ACMEHOME/webroot
+
+(
+cat <<EOF
+server.modules = ( "mod_accesslog" )
+server.document-root = "$ACMEHOME/webroot"
+server.port = 80
+server.bind = "$WANIP"
+server.pid-file = "$ACMEHOME/lighttpd.pid"
+accesslog.filename = "$ACMEHOME/lighttpd.log"
+EOF
+) >$ACMEHOME/lighttpd.conf
+
+/usr/sbin/lighttpd -f $ACMEHOME/lighttpd.conf
+
 /sbin/iptables -I INPUT 1 -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
-$ACMEHOME/acme.sh --issue -d $DOMAIN --standalone --home $ACMEHOME --local-address $WANIP --keypath /tmp/server.key --fullchainpath /tmp/full.cer --reloadcmd /config/scripts/reload.acme.sh
+$ACMEHOME/acme.sh --issue -d $DOMAIN -w $ACMEHOME/webroot --home $ACMEHOME --local-address $WANIP --keypath /tmp/server.key --fullchainpath /tmp/full.cer --reloadcmd /config/scripts/reload.acme.sh
 /sbin/iptables -D INPUT 1
+
+if [ -e "$ACMEHOME/lighttpd.pid" ]
+then
+    kill -s INT $(cat $ACMEHOME/lighttpd.pid)
+fi
