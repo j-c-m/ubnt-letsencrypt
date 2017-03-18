96edec8815e687be1b26cf228f6f4ba9ec5d4f5d
Ken Perkins <ken.perkins@rackspace.com>
2017-03-18 10:50:31 -0700

    Adding support for multiple domains
    - Broke out domains into a "-d mydomain.com" syntax
    - Moved WAN device to an "-i eth0" option
    - Added checks for non existant flags

--- scripts/renew.acme.sh
+++ scripts/renew.acme.sh
@@ -1,13 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $# -ne 2 ]
-then
-    echo "Usage: $0 <domain> <wandev>"
-    exit 1
-fi
+usage() {
+    echo "Usage: $0 -d <mydomain.com> [-d <additionaldomain.com>] -i <wandev>" 1>&2; exit 1;
+}
 
-DOMAIN=$1
-WAN=$2
+# first parse our options
+while getopts "hd:i:" opt; do
+    case $opt in
+        d) DOMAIN+=("$OPTARG");;
+        i) WAN=$OPTARG;;
+        *)
+          usage
+          ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# prepare our domain flags for acme.sh
+for val in "${DOMAIN[@]}"; do
+     DOMAINARG+="-d $val "
+done
+
+if [ ${#DOMAIN[@]} -eq 0 ] || [ -z ${WAN+x} ]; then
+    usage
+fi 
 
 ACMEHOME=/config/.acme.sh
 WANIP=$(ip addr show $WAN | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
@@ -28,7 +44,7 @@ EOF
 /usr/sbin/lighttpd -f $ACMEHOME/lighttpd.conf
 
 /sbin/iptables -I INPUT 1 -p tcp -m comment --comment TEMP_LETSENCRYPT -m tcp --dport 80 -j ACCEPT
-$ACMEHOME/acme.sh --issue -d $DOMAIN -w $ACMEHOME/webroot --home $ACMEHOME --local-address $WANIP --keypath /tmp/server.key --fullchainpath /tmp/full.cer --reloadcmd /config/scripts/reload.acme.sh
+$ACMEHOME/acme.sh --issue $DOMAINARG -w $ACMEHOME/webroot --home $ACMEHOME --local-address $WANIP --keypath /tmp/server.key --fullchainpath /tmp/full.cer --reloadcmd /config/scripts/reload.acme.sh
 /sbin/iptables -D INPUT 1
 
 if [ -e "$ACMEHOME/lighttpd.pid" ]
