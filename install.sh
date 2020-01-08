7d25cd617abd7992d3c84aaea8ad577e601dc170
ozphb <59634675+ozphb@users.noreply.github.com>
2020-01-08 21:30:37 +1000

    changed install to point to local urls

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /config/.acme.sh /config/scripts
 curl -o /config/.acme.sh/acme.sh https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh
-curl -o /config/scripts/renew.acme.sh https://raw.githubusercontent.com/j-c-m/ubnt-letsencrypt/master/renew.acme.sh
+curl -o /config/scripts/renew.acme.sh https://raw.githubusercontent.com/ozphb/ubnt-letsencrypt/master/renew.acme.sh
 chmod 755 /config/.acme.sh/acme.sh /config/scripts/renew.acme.sh
