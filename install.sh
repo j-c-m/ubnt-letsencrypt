a5e3fdf10656f3eabe7a24951979ed4ff4d5fd8d
ohiogeek <62385769+ohiogeek@users.noreply.github.com>
2020-03-19 09:08:19 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p /config/.acme.sh /config/scripts
-curl -o /config/.acme.sh/acme.sh https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh
-curl -o /config/scripts/renew.acme.sh https://raw.githubusercontent.com/j-c-m/ubnt-letsencrypt/master/renew.acme.sh
-chmod 755 /config/.acme.sh/acme.sh /config/scripts/renew.acme.sh
