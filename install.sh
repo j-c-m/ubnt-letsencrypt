3e751c481b19a2293a32747946700e3093371618
Jesse Miller <jmiller@jmiller.com>
2023-11-12 14:12:50 -0700

    ${ACMEHOM} -> ${ACMEHOME}

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ CA_BUNDLE=/config/ssl/cacert.pem
 mkdir -p ${ACMEHOME} /config/scripts
 curl -o ${ACMEHOME}/acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
 curl -o /config/scripts/renew.acme.sh https://raw.githubusercontent.com/j-c-m/ubnt-letsencrypt/master/renew.acme.sh
-chmod 755 ${ACMEHOM}/acme.sh /config/scripts/renew.acme.sh
+chmod 755 ${ACMEHOME}/acme.sh /config/scripts/renew.acme.sh
 
 if [ -s ${CA_BUNDLE} ]; then
     curl https://curl.se/ca/cacert.pem -z ${CA_BUNDLE} -o ${CA_BUNDLE}
